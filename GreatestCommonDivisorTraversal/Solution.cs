481821047b285446aee7e43305f1bff600020b16
ADMIN <ADMIN@DMITRIY-TERESHC>
2024-02-25 15:34:18 +0300

    Add project files.

---
+++ GreatestCommonDivisorTraversal/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatestCommonDivisorTraversal
+{
+	public class Solution
+	{
+		public bool CanTraverseAllPairs(int[] nums)
+		{
+		}
+	}
+}
