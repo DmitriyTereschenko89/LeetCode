2539117e1bca357650944bee429e15a5d8693e33
Дмитрий Терещенко <dmitriy.tereshchenko1989@gmail.com>
2024-03-05 11:56:41 +0300

    add new project from leetcode

---
+++ MinimumLengthStringAfterDeletingSimilarEnds/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace MinimumLengthStringAfterDeletingSimilarEnds
+{
+	public class Solution
+	{
+		public int MinimumLength(string s)
+		{
+			int l = 0;
+			int r = s.Length - 1;
+			while (l <= r)
+			{
+				if (s[l] == s[r])
+				{
+					char letter = s[l];
+					while (l <= r && s[l] == letter)
+					{
+						++l;
+					}
+					while (l <= r && s[r] == letter)
+					{
+						--r;
+					}
+				}
+				else
+				{
+					break;
+				}
+				
+			}
+			return r - l + 1;
+		}
+	}
+}
