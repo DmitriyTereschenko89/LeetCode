3b63621b05cc8ae839964291c3b15e05889354f1
Дмитрий Терещенко <dmitriy.tereshchenko1989@gmail.com>
2024-03-25 11:33:48 +0300

    add tasks

---
+++ Find All Duplicates in an Array/Program.cs
@@ -0,0 +1,16 @@
+﻿using Find_All_Duplicates_in_an_Array;
+
+int tests = int.Parse(Console.ReadLine());
+Solution solution = new();
+List<string> ans = [];
+for (int t = 0; t < tests; t++)
+{
+	string[] nums = Console.ReadLine().Trim().Split(' ');
+	int[] arr = new int[nums.Length];
+	for (int i = 0; i < nums.Length; i++)
+	{
+		arr[i] = int.Parse(nums[i]);
+	}
+	ans.Add(string.Join(", ", solution.FindDuplicates(arr)));
+}
+Console.WriteLine(string.Join("\n", ans));
---
+++ Find All Duplicates in an Array/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace Find_All_Duplicates_in_an_Array
+{
+	public class Solution
+	{
+		public IList<int> FindDuplicates(int[] nums)
+		{
+			List<int> ans = [];
+			for (int i = 0; i < nums.Length; ++i)
+			{
+				int num = Math.Abs(nums[i]);
+				if (nums[num - 1] < 0)
+				{
+					ans.Add(num);
+					continue;
+				}
+				nums[num - 1] *= -1;
+			}
+			return ans;
+		}
+	}
+}
